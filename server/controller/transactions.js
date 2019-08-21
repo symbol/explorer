ba1404609d9fed40f86dd2c887e8cd11b218398c
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-08-21 16:38:47 +0800

    get transaction by hash - supported Transfer, RegisterNamespace, MosaicAlias, MosaicDefinition, MosaicSupplyChange

--- server/controller/transactions.js
+++ server/controller/transactions.js
@@ -1,5 +1,6 @@
 const { TransactionHttp } = require('nem2-sdk');
 const utils = require('../utils');
+const request = require('request');
 
 const endpoint = utils.getNodeEndPoint();
 
@@ -8,17 +9,30 @@ const transactionHttp = new TransactionHttp(endpoint);
 async function getTransactionInfoByHash(hash) {
 	// Todo getTransaction
 	// const transaction = await transactionHttp.getTransaction(hash).toPromise(); // waiting SDK fix duel rest change
+	const transaction = await getTransaction(hash);
+
 	const transactionStatus = await transactionHttp
 		.getTransactionStatus(hash)
 		.toPromise();
 
 	const transactionInfo = {
 		// transaction: utils.formatTransaction(transaction),
+		transaction: utils.formatTransactionFromApi(transaction),
 		status: transactionStatus.status,
 		confirm: transactionStatus.group,
 	};
 
 	return transactionInfo;
+}
+
+function getTransaction(hash) {
+	return new Promise((resolve, reject) => {
+		request(endpoint + '/transaction/' + hash, (error, response, body) => {
+			if (error) return reject(error);
+			let info = JSON.parse(body);
+			return resolve(info);
+		});
+	});
 }
 
 module.exports = {
