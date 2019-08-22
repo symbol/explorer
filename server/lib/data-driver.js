e88752e38bcb3576b369a0ef1386d1c47085a18f
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-08-22 18:01:04 +0800

    minor fix

--- server/lib/data-driver.js
+++ server/lib/data-driver.js
@@ -1,30 +1,19 @@
-const request = require("request");
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
-const axios = require('axios');
-const nem2 = require('nem2-sdk');
-var mongodb = require('mongodb');
+const mongodb = require('mongodb');
 
-
-
-
-
-// getCollectionData('networkinfo').then(function(data){
-//     console.log(data);
-// });
-
+getCollectionData('networkinfo').then(function(data) {
+	console.log(data);
+});
 
 //CRUD OPS
 //read
 async function getCollectionData(collection) {
-    let temp = await loadDatabaseCollection(collection);
-    return Promise.resolve( temp.find({}).toArray());
+	let temp = await loadDatabaseCollection(collection);
+	return Promise.resolve(temp.find({}).toArray());
 }
 // create
-async function addCollectionRow(data,collection){
-    let temp = await loadDatabaseCollection(collection);
-    return Promise.resolve(  temp.insertOne(data) ); //deleteMany({} ));
+async function addCollectionRow(data, collection) {
+	let temp = await loadDatabaseCollection(collection);
+	return Promise.resolve(temp.insertOne(data)); //deleteMany({} ));
 }
 //deleteAll dev purpose
 
@@ -35,9 +24,8 @@ async function addCollectionRow(data,collection){
 
 // connect and load collection
 async function loadDatabaseCollection(collection = null) {
-    const client = await mongodb.MongoClient.connect
-        (conf.db_connect_string, {
-            useNewUrlParser: true
-        })
-    return client.db(conf.db_name).collection(collection);
+	const client = await mongodb.MongoClient.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+	});
+	return client.db(process.env.DB_NAME).collection(collection);
 }
