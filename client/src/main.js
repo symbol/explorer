71d6011d7e1947e5cd532c680c36a231b9704aae
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2019-08-23 10:26:06 -0500

    Created slot component for TimeSince for calculating ages.

--- client/src/main.js
+++ client/src/main.js
@@ -6,12 +6,14 @@ import store from './store'
 import TopHead from '@/components/TopHead.vue'
 import PageMenu from '@/components/PageMenu.vue'
 import Pagefooter from '@/components/PageFooter.vue'
+import TimeSince from '@/components/TimeSince.vue'
 import helper from "./helper";
 
 Vue.config.productionTip = false
 Vue.component('top-header', TopHead)
 Vue.component('page-menu', PageMenu)
 Vue.component('page-footer', Pagefooter)
+Vue.component('time-since', TimeSince)
 
 var settings = {
   networktime: 1459468800000,
