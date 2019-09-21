f3506b2a24546c1f4722f7de67181d941da3d647
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-09-21 15:31:52 +0800

    merged

--- client/src/views/Blocks.vue
+++ client/src/views/Blocks.vue
@@ -32,69 +32,6 @@
                 </div>
               </div>
               <div class="box-con mt-0">
-                <!-- <loader v-if="!this.loading"></loader>
-                <div class="table-responsive">
-                  <div
-                    id="sorting-table_wrapper"
-                    class="dataTables_wrapper container-fluid dt-bootstrap4 no-footer p-0"
-                  >
-                    <table
-                      id="table-block-list"
-                      class="table table-striped table-bordered"
-                      cellspacing="0"
-                      width="100%"
-                    >
-                      <thead>
-                        <tr>
-                          <th>Block Height</th>
-                          <th>Age</th>
-                          <th>Transactions</th>
-                          <th>Fee</th>
-                          <th>Timestamp</th>
-                          <th>Harvester</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr v-for="item in getLatestBlockList" v-bind:key="item.height">
-                          <td>
-                            <router-link :to="'/block/' + item.height">{{item.height}}</router-link>
-                          </td>
-                          <td>
-                            <time-since :date="item.date">
-                              <template slot-scope="interval">
-                                {{timeSince(interval)}}
-                              </template>
-                            </time-since>
-                          </td>
-                          <td>{{item.numTransactions}}</td>
-                          <td>{{item.totalFee}}</td>
-                          <td>{{item.date}}</td>
-                          <td>
-                            <router-link
-                              :to="'/account/' + item.signer.address.address"
-                            >{{item.signer.address.address}}</router-link>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-                <div class="table-footer">
-                  <div class="pagination-container">
-                    <ul class="pagination">
-                      <li class="page-item" @click="previousPage()">
-                        <a href="#">
-                          <i class="ico-angle-left"></i>
-                        </a>
-                      </li>
-                      <li class="page-item">
-                        <a href="#" @click="nextPage()">
-                          MORE
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div> -->
                 <loader v-if="loading"></loader>
                 <BlockTable :blockList="blockList"></BlockTable>
                 <Pagination class="table-footer"
