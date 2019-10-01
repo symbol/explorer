4ff218a7b96aeadab0d8e856eeda2db39ca579c2
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-10-01 23:44:24 +0800

    updated params

--- client/src/router.js
+++ client/src/router.js
@@ -43,7 +43,7 @@ export default new Router({
       }
     },
     {
-      path: '/block/:blockId',
+      path: '/block/:height',
       name: 'block-detail',
       component: () =>
         import('./views/BlockDetail.vue'),
