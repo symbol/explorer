53bf75e5162afacde34128b185bea1c2297914af
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-10-05 15:07:23 +0800

    updated vue configuration and package

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const path = require('path')
 module.exports = {
-  outputDir: path.resolve(__dirname, 'public'),
+  // base url
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './'
+    : '/',
+  // output dir
+  outputDir: './www',
+  // eslint-loader check
+  lintOnSave: true,
+  // webpack-dev-server
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    before: app => {
+    }
+  },
 }
