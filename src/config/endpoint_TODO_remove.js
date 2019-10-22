241ac5b37149888a8bb3eb032c37a211034edf45
OlegMakarenko <33131259+OlegMakarenko@users.noreply.github.com>
2019-10-22 23:22:57 +0300

    travis fix

--- src/config/endpoint_TODO_remove.js
+++ src/config/endpoint_TODO_remove.js
@@ -1,86 +1,86 @@
-import peersApi from '../config/peers-api.json';
-import endpoints from '../config/endpoints.json';
+// import peersApi from '../config/peers-api.json';
+// import endpoints from '../config/endpoints.json';
 
 
-const DEFAULT_NODE = peersApi.defaultNode;
-const MARKET_DATA_URL = endpoints.MARKET_DATA;
-const nodes = peersApi.nodes;
-
-
-
-const getCurrentNode = (nodes) => {
-    const currentNodeIndex = localStorage.getItem('currentNodeIndex');
-    if(
-        nodes
-        &&currentNodeIndex !== null 
-        && currentNodeIndex !== void 0 
-        && nodes[currentNodeIndex]
-    )
-        return nodes[currentNodeIndex];
-    else
-        return DEFAULT_NODE;
-} 
-
-const setCurrentNode = (index, nodes) => {
-    if(
-        nodes
-        &&index !== null 
-        && index !== void 0 
-        && nodes[index]
-    ) 
-        localStorage.setItem('currentNodeIndex', index);
-    location.reload();
-} 
-
-const getNodes = (nodes) => {
-    return nodes.map( node => {
-        let url = new URL(node.url)
-        return {
-            protocol: url.protocol, 
-            hostname: url.hostname,
-            port: url.port,
-            url: node.url
-        }
-    })
-}
-
-const getCurrentNodeHostname = (nodes) => {
-    return new URL (getCurrentNode(nodes).url).hostname
-}
+// const DEFAULT_NODE = peersApi.defaultNode;
+// const MARKET_DATA_URL = endpoints.MARKET_DATA;
+// const nodes = peersApi.nodes;
 
 
 
-class Endpoint {
-    constructor () {
-        console.log(window.config)
-        this.nodes = window.config?.nodes 
-            ? window.config.nodes 
-            : nodes;
-    }
-    get currentNode() { return getCurrentNode(this.nodes) }
-    get currentNodeHostname() { return getCurrentNodeHostname(this.nodes) }
-    setCurrentNodeByIndex(index) { return setCurrentNode(index, this.nodes) }
+// const getCurrentNode = (nodes) => {
+//     const currentNodeIndex = localStorage.getItem('currentNodeIndex');
+//     if(
+//         nodes
+//         &&currentNodeIndex !== null 
+//         && currentNodeIndex !== void 0 
+//         && nodes[currentNodeIndex]
+//     )
+//         return nodes[currentNodeIndex];
+//     else
+//         return DEFAULT_NODE;
+// } 
 
-    get nodesFormatted() { return getNodes(this.nodes) }
+// const setCurrentNode = (index, nodes) => {
+//     if(
+//         nodes
+//         &&index !== null 
+//         && index !== void 0 
+//         && nodes[index]
+//     ) 
+//         localStorage.setItem('currentNodeIndex', index);
+//     location.reload();
+// } 
 
-    get ws() { return (
-            getCurrentNode().url.replace('http', 'ws')
-        )
-    }
+// const getNodes = (nodes) => {
+//     return nodes.map( node => {
+//         let url = new URL(node.url)
+//         return {
+//             protocol: url.protocol, 
+//             hostname: url.hostname,
+//             port: url.port,
+//             url: node.url
+//         }
+//     })
+// }
 
-    get marketDataURL() { return MARKET_DATA_URL }
+// const getCurrentNodeHostname = (nodes) => {
+//     return new URL (getCurrentNode(nodes).url).hostname
+// }
 
-    get api() { return (
-            console.log("store", store)
-            //getCurrentNode().url 
-        )
-    }
-}
 
-export default {
-    get api() {
-        console.log("store", window.store?.getters['api/currentNode']?.url);
-        return window.store?.getters['api/currentNode']?.url
-    }
-}
-//new Endpoint;
+
+// class Endpoint {
+//     constructor () {
+//         console.log(window.config)
+//         this.nodes = window.config?.nodes 
+//             ? window.config.nodes 
+//             : nodes;
+//     }
+//     get currentNode() { return getCurrentNode(this.nodes) }
+//     get currentNodeHostname() { return getCurrentNodeHostname(this.nodes) }
+//     setCurrentNodeByIndex(index) { return setCurrentNode(index, this.nodes) }
+
+//     get nodesFormatted() { return getNodes(this.nodes) }
+
+//     get ws() { return (
+//             getCurrentNode().url.replace('http', 'ws')
+//         )
+//     }
+
+//     get marketDataURL() { return MARKET_DATA_URL }
+
+//     get api() { return (
+//             console.log("store", store)
+//             //getCurrentNode().url 
+//         )
+//     }
+// }
+
+// export default {
+//     get api() {
+//         console.log("store", window.store?.getters['api/currentNode']?.url);
+//         return window.store?.getters['api/currentNode']?.url
+//     }
+// }
+// //new Endpoint;
