27edeec96c0a95274db65593ef5bf94de9a441dd
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-10-24 17:18:01 +0800

    added aggregate Transaction component

---
+++ src/components/AggregateTransaction.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <TableInfoView :data="data" />
+    <TableListView :data="data.mosaics" />
+  </div>
+</template>
+
+<script>
+import View from '../views/View'
+
+export default {
+  extends: View,
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
