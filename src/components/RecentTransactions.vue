ce04f5e28e4d7001bb55c7aaf4321713e0686d90
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2019-11-19 09:50:55 -0600

    Removed the use of global components and fixed component naming.
    1. Removed global, registered components.
        - Mostly already registered under `View.vue`.
    2. Removed redundant components in `NamespaceDetail.vue`
        - `TableInfoView`
        - `TableListView`
    3. Renamed `TopHead.vue` to `TopHeader.vue`
        - Consistent with the previously used name.

--- src/components/RecentTransactions.vue
+++ src/components/RecentTransactions.vue
@@ -37,7 +37,7 @@
                 </div>
             </div>
             <div class="box-con">
-                <loader
+                <Loading
                     v-if="loading"
                 />
                 <div class="row">
@@ -52,6 +52,7 @@
     </div>
 </template>
 <script>
+import Loading from '@/components/Loading.vue'
 import RecentTransactionRow from '@/components/RecentTransactionRow.vue'
 import { mapGetters } from 'vuex'
 
@@ -59,6 +60,7 @@ export default {
   name: 'RecentTransactions',
 
   components: {
+    Loading,
     RecentTransactionRow
   },
 
