700b0f0777745f3f6cbf4413f4aa8fafd7f3fb0a
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-11-26 21:00:55 +0800

    fix #256

--- src/helper.js
+++ src/helper.js
@@ -55,7 +55,7 @@ class helper {
     if (typeof _str === 'string') {
       let str = _str.replace('ws', 'http')
       let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
         '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
