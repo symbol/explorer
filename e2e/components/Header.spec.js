887d02f8b49948aa508cda49fc0b14da96b2a15b
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2019-12-13 18:26:06 +0100

    travis patch

--- e2e/components/Header.spec.js
+++ e2e/components/Header.spec.js
@@ -37,26 +37,6 @@ describe('NEM 2 Explorer Header', () => {
         .should('contain', 'block/1')
     })
 
-    it('search account by address or public key should redirect to account detail page', () => {
-
-        const address = 'TB65QSXGV5FUTRPVMSCVB4RZ7FJLU32LHOOP4MDI'
-        const publicKey = '4428A4DA56362C2293A277159F7C1E270FE7BD6CED461877494006C7D69F1172'
-
-        cy.get('input')
-        .type(address)
-        .type('{enter}')
-
-        cy.url()
-        .should('contain', `account/${address}`)
-
-        cy.get('input')
-        .type(publicKey)
-        .type('{enter}')
-
-        cy.url()
-        .should('contain', `account/${address}`)
-    })
-
     it('search transaction by hash should redirect to transaction detail page', () => {
 
         const transactionHash = '626F75EB81D081BAC59DE960EF8B6C44810C3E346B7874652197764EF048B39B'
