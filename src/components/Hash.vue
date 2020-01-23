daf58105aacfeba8ca98643b462199ea72eb12b4
Oleh Makarenko <olegomm@gmail.com>
2020-01-23 20:20:13 +0200

    Truncate hash and TableListView link text selection fix

---
+++ src/components/Hash.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="d-none d-md-block">
+            <slot>
+            </slot>
+        </div>
+        <div class="hash-mobile d-block d-md-none">
+            <slot>
+            </slot>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.hash-mobile{
+    text-overflow: ellipsis;
+    overflow: hidden; 
+    white-space: nowrap;
+    width: 50px;
+}
+</style>
