3884ee3e4c2225f8dc14878af81cd61f6a6cf84c
Oleh Makarenko <olegomm@gmail.com>
2020-01-27 15:24:05 +0200

    Mobile view Hash: dots in the middle

--- src/components/Hash.vue
+++ src/components/Hash.vue
@@ -1,21 +1,43 @@
 <template>
     <div>
         <div class="d-none d-md-block">
-            <slot>
-            </slot>
+            <slot />
         </div>
         <div class="hash-mobile d-block d-md-none">
-            <slot>
-            </slot>
+            <span><slot /></span>
+            <span><slot /></span>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .hash-mobile {
-    text-overflow: ellipsis;
-    overflow: hidden;
-    white-space: nowrap;
-    width: 50px;
+    span {
+        white-space: nowrap;
+        overflow: hidden;
+        display:inline-block;
+    }
+
+    span:first-child {
+        width: 70px;
+        text-overflow: ellipsis;
+        -webkit-mask-image: linear-gradient(to left, rgba(0,0,0,0.3), rgba(0,0,0,1));
+        -webkit-mask-size: 100% 100%;
+        -webkit-mask-repeat: no-repeat;
+        -webkit-mask-position: left top;
+    }
+
+    span + span {
+        width: 40px;
+        direction:rtl;
+        text-align:right;
+        -webkit-mask-image: linear-gradient(to left, rgba(0,0,0,1), rgba(0,0,0,0));
+        -webkit-mask-size: 100% 100%;
+        -webkit-mask-repeat: no-repeat;
+        -webkit-mask-position: left top;
+    }
+    // text-overflow: ellipsis;
+    // overflow: hidden;
+    // white-space: nowrap;
 }
 </style>
