10913687f881d1ef4ec705f1bf7dbaeebe6b6ef2
Oleh Makarenko <olegomm@gmail.com>
2020-01-28 21:54:31 +0200

    e2e: add timeouts

--- e2e/pages/home.spec.js
+++ e2e/pages/home.spec.js
@@ -13,7 +13,7 @@ describe('NEM 2 Explorer Home Page should', () => {
     // cy.url().should('contain', '/statistics')
 
     // xem price chart is visible
-    cy.get('.apexcharts-svg').should('be.visible')
+    cy.get('.apexcharts-svg', { timeout: 20000 }).should('be.visible')
 
   })
 
@@ -37,7 +37,8 @@ describe('NEM 2 Explorer Home Page should', () => {
   it('load Recent Transactions', () => {
     cy.contains('Recent Transactions')
 
-    cy.contains('View all transactions')
+    
+    cy.contains('View all transactions', { timeout: 20000 })
     .click()
 
     cy.url()
