db892da90fa2a2813bf8e5e431b520d57f034d06
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-03-25 18:30:24 +0800

    update transactions list e2e test

--- e2e/pages/transactions.spec.js
+++ e2e/pages/transactions.spec.js
@@ -1,12 +1,13 @@
 describe('Symbol Explorer Transactions list page should', () => {
-    it('load pages titles', () => {
+    beforeEach(() => {
         cy.visit('/transactions')
+    })
+
+    it('load pages titles', () => {
         cy.contains('Transactions')
     })
 
     it('render table header and table body', () => {
-        cy.visit('/transactions')
-
         cy.get('table')
         .should('be.visible')
 
@@ -18,15 +19,11 @@ describe('Symbol Explorer Transactions list page should', () => {
     })
 
     it('render 7 items in table header', () => {
-        cy.visit('/transactions')
-
         cy.get('thead > tr > th')
         .should('have.length', 7)
     })
 
     it('render at least 1 row data in the table list', () => {
-        cy.visit('/transactions')
-
         cy.get('tbody > tr')
         .then($data => {
             return $data.length
@@ -34,24 +31,30 @@ describe('Symbol Explorer Transactions list page should', () => {
     })
 
     it('redirect to transaction detail page given click on transaction hash', () => {
-        cy.visit('/transactions')
-
-        cy.get('tbody tr .transactionHash')
+        cy.get('tbody tr .transactionHash a')
         .then($data => {
             return $data[0]
-        }).click()
+        }).click({ force:true })
 
         cy.url().should('contain', '/transaction')
     })
 
     it('redirect to account detail page given click on signer address', () => {
-        cy.visit('/transactions')
-
-        cy.get('tbody tr .signer')
+        cy.get('tbody tr .signer a')
         .then($data => {
             return $data[0]
-        }).click()
+        }).click({ force:true })
 
         cy.url().should('contain', '/account')
+    })
+
+    it('redirect to block detail page given click on block height', () => {
+        cy.get('tbody tr .height a')
+        .then($data => {
+            console.log($data[0])
+            return $data[0]
+        }).click({ force:true })
+
+        cy.url().should('contain', '/block')
     })
   })
