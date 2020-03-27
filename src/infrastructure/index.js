b0a412f299f3f1cb4e6d44fc142b41c102e7e905
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-03-27 18:34:49 +0800

    added RestrictionService

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,7 +1,9 @@
 import NodeService from './NodeService'
 import MetadataService from './MetadataService'
+import RestrictionService from './RestrictionService'
 
 export {
   NodeService,
-  MetadataService
+  MetadataService,
+  RestrictionService
 }
