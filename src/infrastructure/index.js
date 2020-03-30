79bb1dbef4b0b9f26ec59a8453e49f794cf8bfa7
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-03-30 17:29:36 +0800

    added chain service

--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -2,10 +2,12 @@ import NodeService from './NodeService'
 import MetadataService from './MetadataService'
 import RestrictionService from './RestrictionService'
 import ListenerService from './ListenerService'
+import ChainService from './ChainService'
 
 export {
   NodeService,
   MetadataService,
   RestrictionService,
-  ListenerService
+  ListenerService,
+  ChainService
 }
