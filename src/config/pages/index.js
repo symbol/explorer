512f20a3b19bbdb0f5f3df3b0efcb1090902d366
Oleh Makarenko <olegomm@gmail.com>
2020-06-10 21:41:29 +0300

    [PageAssembler] Move List-Pages to config/pages

--- src/config/pages/index.js
+++ src/config/pages/index.js
@@ -1,5 +1,9 @@
 export default [
     require('./detail-page-example'),
+    require('./account-list'),
     require('./block-list'),
-    require('./transaction-list'),
+    require('./mosaic-list'),
+    require('./namespace-list'),
+    require('./node-list'),
+    require('./transaction-list')
 ]
