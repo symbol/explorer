d945a4e6c963b7fb1b37ef12152b2593c2d8a75d
Oleh Makarenko <olegomm@gmail.com>
2020-06-17 19:11:05 +0300

    Create TransactionSchema component template

---
+++ src/components/TransactionSchema.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <svg>
+            
+        </svg>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
