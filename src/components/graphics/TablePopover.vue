17f48136ed6297ccd6a69e7272cf4a585f30aca4
Oleh Makarenko <olegomm@gmail.com>
2020-07-13 16:59:49 +0300

    add TablePopover

---
+++ src/components/graphics/TablePopover.vue
@@ -0,0 +1,56 @@
+<template>
+    <b-popover :target="target" placement="bottom" triggers="hover">
+        <template v-slot:title>{{ title }}</template>
+        <b-list-group>
+            <b-list-group-item
+                v-for="(value, key) in table"
+                class="d-flex justify-content-between align-items-center table-list"
+                :key="'tp' + value + key"
+                :title="key + ': ' + value"
+            >
+                <span>
+                    {{ truncString(key, 5) }} :
+                </span>
+                <span>
+                    {{ truncString(value, 5) }}
+                </span>
+            </b-list-group-item>
+        </b-list-group>
+    </b-popover>
+</template>
+
+<script>
+import NamespaceIcon from '../graphics/NamespaceIcon.vue'
+import GraphicComponent from './GraphicComponent.vue'
+
+export default {
+  extends: GraphicComponent,
+
+  components: {
+    NamespaceIcon
+  },
+
+  props: {
+    table: {
+      type: Object,
+      default: () => ({})
+    },
+
+    title: {
+      type: String,
+      default: 'Table'
+    },
+
+    target: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.table-list {
+    min-width: 250px;
+}
+</style>
