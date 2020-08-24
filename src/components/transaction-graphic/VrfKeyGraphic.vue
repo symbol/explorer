d07e5a3434ddd17cc8793c883de8e6378c9ca968
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-08-24 19:42:15 +0800

    added vrf key graphic

---
+++ src/components/transaction-graphic/VrfKeyGraphic.vue
@@ -0,0 +1,126 @@
+<template>
+	<div>
+		<svg
+			version="1.1"
+			xmlns="http://www.w3.org/2000/svg"
+			xmlns:xlink="http://www.w3.org/1999/xlink"
+			x="0px"
+			y="0px"
+			:width="getPixels(transactionGraphicWidth)"
+			:height="getPixels(transactionGraphicHeight)"
+			:viewBox="transactionGraphicViewbox"
+			xml:space="preserve"
+		>
+			<AccountIcon
+				:x="subjectPositionX"
+				:y="subjectPositionY"
+				:width="subjectWidth"
+				:height="subjectHeight"
+				:address="signer"
+			/>
+			<AccountIcon
+				:x="objectPositionX"
+				:y="objectPositionY"
+				:width="subjectWidth"
+				:height="subjectHeight"
+				:address="linkedAccountAddress"
+			/>
+			<Arrow :x="arrowPositionX" :y="arrowPositionY" />
+			<KeyLinkCircle
+				v-if="isLinkAction"
+				:x="getCircleIconPositionX(0)"
+				:y="circleIconPositionY"
+				title="VRF Key Link"
+				:data="keyLinkInfo"
+			/>
+			<KeyUnlinkCircle
+				v-else
+				:x="getCircleIconPositionX(0)"
+				:y="circleIconPositionY"
+				title="VRF Key Link"
+				:data="keyLinkInfo"
+			/>
+			<text :x="transactionTypeTextPositionX" :y="transactionTypeTextPositionY" text-anchor="middle" class="message">
+				{{ transactionType + subTitle }}
+				<title>{{ transactionType }}</title>
+			</text>
+		</svg>
+	</div>
+</template>
+
+<script>
+import GraphicComponent from '../graphics/GraphicComponent.vue';
+import AccountIcon from '../graphics/AccountIcon.vue';
+import KeyLinkCircle from '../graphics/KeyLinkCircle.vue';
+import KeyUnlinkCircle from '../graphics/KeyUnlinkCircle.vue';
+import Arrow from '../graphics/Arrow.vue';
+
+export default {
+	extends: GraphicComponent,
+
+	components: {
+		AccountIcon,
+		Arrow,
+		KeyLinkCircle,
+		KeyUnlinkCircle
+	},
+
+	props: {
+		message: {
+			type: String,
+			default: ''
+		},
+		signer: {
+			type: String,
+			required: true,
+			default: ''
+		},
+		linkedAccountAddress: {
+			type: String,
+			required: true,
+			default: ''
+		},
+		linkAction: {
+			type: String,
+			required: true,
+			default: ''
+		},
+		linkedPublicKey: {
+			type: String,
+			required: true,
+			default: ''
+		}
+	},
+
+	data() {
+		return {
+			width: this.transactionGraphicWidth,
+			heigth: this.transactionGraphicHeight
+		};
+	},
+
+	computed: {
+		transactionType() {
+			return this.getTransactionTypeCaption(16963);
+		},
+
+		circleIconsToDisplay() {
+			return [true];
+		},
+
+		isLinkAction() {
+			return this.linkAction === 'Link';
+		},
+
+		subTitle() {
+			return `. ${this.linkAction} account`;
+		},
+
+		keyLinkInfo() {
+			return {
+				publicKey: this.linkedPublicKey
+			}
+		}
+	}
+};
+</script>
