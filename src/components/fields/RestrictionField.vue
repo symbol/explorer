31a31c02363d505acfb0e519a5d8fd397d5ba4b4
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-09-16 15:02:14 +0800

    added restriction field compoment

---
+++ src/components/fields/RestrictionField.vue
@@ -0,0 +1,70 @@
+/*
+ *
+ * Copyright (c) 2019-present for NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License ");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+<template>
+	<div class="restriction-tag-container">
+		<span
+			v-for="(item, index) in value"
+			class="restriction"
+			:key="'restriction_key_' + index"
+			:title="'restriction type: ' + item.restrictionType + ' | restriction value: ' + item.restrictionValue"
+		>
+
+			<span class="restriction-content">
+				Key {{ item.restrictionKey }} {{ item.restrictionType || ':' }} {{ item.restrictionValue }}
+			</span>
+		</span>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'RestrictionField',
+
+	props: {
+		value: {
+			type: Array,
+			required: true
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.restriction-tag-container {
+    width: 100%;
+    overflow-x: auto;
+    overflow-y: hidden;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+
+    .restriction {
+        display: inline-block;
+        background: $primary-color;
+        color: #fff;
+        border-radius: 5px;
+        border-width: 1px;
+        border-style: solid;
+        border-color: $primary-color;
+        padding: 5px 10px;
+        margin-right: 10px;
+        white-space: nowrap;
+    }
+}
+</style>
