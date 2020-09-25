41bc846b9ea579664881e82af4b67b8e382c27e6
Oleh Makarenko <olegomm@gmail.com>
2020-09-25 17:29:13 +0300

    add component AccountListItem

---
+++ src/components/graphics/AccountListItem.vue
@@ -0,0 +1,58 @@
+<template>
+    <b-list-group-item
+        class="d-flex justify-content-between align-items-center mosaic-list"
+        :title="title"
+    >
+        <AccountIcon
+            hideCaption
+            :width="32"
+            :height="32"
+            :address="account.address"
+        />
+        {{ text }}
+        <b-badge v-if="isValueExist" variant="primary" pill>{{ _value }}</b-badge>
+		<div v-else> &nbsp; </div>
+    </b-list-group-item>
+</template>
+
+<script>
+import AccountIcon from '../graphics/AccountIcon.vue';
+import GraphicComponent from './GraphicComponent.vue';
+
+export default {
+	extends: GraphicComponent,
+
+	components: {
+		AccountIcon
+	},
+
+	props: {
+		account: {
+			type: Object,
+			default: () => ({})
+		},
+
+		value: {
+			type: [Number, String]
+		}
+	},
+
+	computed: {
+		text() {
+			return this.truncString(this.account.address, 5);
+		},
+
+		title() {
+			return this.account.address;
+		},
+
+		isValueExist() {
+			return typeof this.value === 'number' || typeof this.value === 'string';
+		},
+
+		_value() {
+			return this.value || this.address.amount;
+		}
+	}
+}
+</script>
