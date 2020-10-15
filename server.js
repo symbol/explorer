3d0cb7e9a1bd70ff0f4d9178b13c2cb718c25815
Oleh Makarenko <olegomm@gmail.com>
2020-10-15 17:17:50 +0300

    add server

---
+++ server.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const http = require('http');
+
+const PORT = 4000;
+const STATIC_FOLDER = '/www';
+const INDEX_HTML = '/index.html';
+const ENV = JSON.stringify(process.env);
+
+
+
+const getFile = (url, errCallback, callback) => {
+	fs.readFile(__dirname + STATIC_FOLDER + url, (err, data) => {
+		if (err) {
+			if(typeof errCallback === 'function')
+				errCallback(err);
+		}
+		else
+			if(typeof callback === 'function')
+				callback(data);
+	});
+};
+
+const send = (res, data) => {
+	res.writeHead(200);
+	res.end(data);
+};
+
+const sendJSON = (res, data) => {
+	res.setHeader('Content-Type', 'application/json');
+	send(res, data);
+}
+
+const sendError = (res, err, code) => {
+	res.writeHead(code);
+	res.end(JSON.stringify(err));
+	return;
+};
+
+http.createServer((req, res) => {
+	if(req.url === '/')
+		req.url = INDEX_HTML;
+
+	if(req.url === '/config') { 
+		sendJSON(res, ENV);
+	}
+	else {
+		getFile(req.url, 
+			() => {
+				getFile(INDEX_HTML, 
+					err => sendError(res, err, 404),
+					data => send(res, data)
+				);
+			},
+			data => send(res, data)
+		);
+	}
+}).listen(PORT);
+
+console.log('Server is running on port: ' + PORT);
