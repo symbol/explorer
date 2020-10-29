cb4f3f12b2b6d492ea15461efcd1008b079095f7
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-10-29 21:14:32 +0800

    Added Extend components

---
+++ src/components/extend-graphic-value/ExtendMosaicDefinitionValue.vue
@@ -0,0 +1,55 @@
+<template>
+	<span
+		v-if="hasMosaicId"
+		:title="getTranslation('mosaicId') + ': ' + mosaicId">
+
+		<MosaicIcon
+			hideCaption
+			:width="16"
+			:height="16"
+			:mosaicId="mosaicId"
+		/>
+
+		{{ truncString(mosaicId) }}
+	</span>
+</template>
+
+<script>
+import GraphicComponent from '@/components/graphics/GraphicComponent.vue';
+import MosaicIcon from '@/components/graphics/MosaicIcon.vue';
+
+export default {
+	extends: GraphicComponent,
+
+	components: {
+		MosaicIcon
+	},
+
+	props: {
+		value: {
+			type: Array,
+			required: true,
+			default: () => []
+		}
+	},
+
+	computed: {
+		hasMosaicId() {
+			for (const item of this.value) {
+				if (Object.keys(item).includes('mosaicId'))
+					return typeof item.mosaicId === 'string';
+			}
+			return false;
+		},
+
+		mosaicId() {
+			for (const item of this.value) {
+				if (Object.keys(item).includes('mosaicId'))
+					return item.mosaicId;
+			}
+
+			return '';
+		}
+	}
+};
+</script>
