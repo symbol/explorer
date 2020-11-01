cbb658b472a723a5e64f175aac2b11fb52ffb095
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-11-02 02:35:03 +0800

    added prettier and script

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,41 +6,18 @@ module.exports = {
     },
 
     extends: [
-        'plugin:vue/essential',
-        '@vue/standard',
-        // 'airbnb-base',
-        // 'plugin:prettier/recommended'
+        'plugin:vue/recommended',
+        'prettier/vue',
+        'plugin:prettier/recommended',
     ],
+
+    plugins: ['vue'],
 
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'curly': ['error', 'multi-or-nest'],
-        'no-useless-escape': 'off',
-        'no-var': 'error',
-        'space-before-function-paren': ['error', {
-            'anonymous': 'always',
-            'named': 'never',
-            'asyncArrow': 'always'
-        }],
-        'indent': ['error', 'tab'],
-        'no-unused-expressions': 'off',
-        'no-tabs': 0,
-        'no-mixed-spaces-and-tabs': 0,
-        'semi': ['error', 'always'],
-        'newline-per-chained-call': ['error'],
-        'brace-style': ['error', 'stroustrup'],
-        // 'array-bracket-newline': ['error', { 'multiline': true, 'minItems': 2 }],
-        'newline-after-var': ['error', 'always'],
-        'vue/html-indent': ['error', 'tab', {
-            'attribute': 1,
-            'baseIndent': 1,
-            'closeBracket': 0,
-            'alignAttributesVertically': false,
-            'ignores': []
-        }]
-        // '@typescript-eslint/no-unused-expressions': 'error'
-
+        'vue/require-default-prop': 'off',
+        'vue/no-v-html': 'off'
     },
 
     globals: {
