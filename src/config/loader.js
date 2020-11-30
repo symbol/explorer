2052345645930d048e89a49556c39d8ef0e82bd1
Oleh Makarenko <olegomm@gmail.com>
2020-12-01 01:36:04 +0200

    upd

--- src/config/loader.js
+++ src/config/loader.js
@@ -5,5 +5,4 @@ const CONFIG_ROUTE = '/config';
 export const loadConfig = () => Axios.get(window.location.origin + CONFIG_ROUTE)
 	.then(res => window.globalConfig = res.data)
 	.catch(() => {});
-
- 
+	
