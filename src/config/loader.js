47def889e5a888e7a4c5843aa8879d67cfce8662
Oleh Makarenko <olegomm@gmail.com>
2020-12-01 02:00:06 +0200

    lint

--- src/config/loader.js
+++ src/config/loader.js
@@ -3,6 +3,5 @@ import Axios from 'axios';
 const CONFIG_ROUTE = '/config';
 
 export const loadConfig = () => Axios.get(window.location.origin + CONFIG_ROUTE)
-	.then(res => window.globalConfig = res.data)
+	.then(res => { window.globalConfig = res.data })
 	.catch(() => {});
-	
