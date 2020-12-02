4ed495ea660dfe2f4255b33fd380eaa4902b9173
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2020-12-02 18:42:04 +0800

    added optional chaining in http

--- src/infrastructure/http.js
+++ src/infrastructure/http.js
@@ -55,8 +55,8 @@ export default class http {
 
   static get networkCurrency() {
   	return {
-  		namespaceName: NETWORK_CURRECY?.currency.namespaceId.fullName || globalConfig.networkConfig.namespaceName,
-  		mosaicId: NETWORK_CURRECY?.currency.namespaceId.toHex() || globalConfig.networkConfig.mosaicId,
+  		namespaceName: NETWORK_CURRECY?.currency.namespaceId?.fullName || globalConfig.networkConfig.namespaceName,
+  		mosaicId: NETWORK_CURRECY?.currency.namespaceId?.toHex() || globalConfig.networkConfig.mosaicId,
   		divisibility: NETWORK_CURRECY?.currency.divisibility || globalConfig.networkConfig.divisibility
   	};
   }
