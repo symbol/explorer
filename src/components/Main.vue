965f802ecda8e16352382a59daf3d80b97acd6b7
Oleh Makarenko <olegomm@gmail.com>
2020-12-21 21:05:30 +0200

    create main page

---
+++ src/components/Main.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="main-root">
+		<History :data="history" />
+	</div>
+</template>
+
+<script>
+import History from './History.vue';
+
+export default {
+	name: 'Main',
+
+	components: { History },
+
+	props: {
+		data: {
+			type: Object,
+			required: true
+		},
+		language: {
+			type: String,
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.main-root {
+	background: transparent;
+	position: relative;
+}
+</style>
