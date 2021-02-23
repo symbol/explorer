bb7d2fef160b53ceee99df4f2187745de5745aab
Oleg Makarenko <olegomm@gmail.com>
2021-02-23 22:19:58 +0200

    fix nrc import

--- src/views/PageAssembler.vue
+++ src/views/PageAssembler.vue
@@ -53,7 +53,7 @@ import RecentBlocksWidget from '@/components/widgets/RecentBlocksWidget.vue';
 import RecentTransactionsWidget from '@/components/widgets/RecentTransactionsWidget.vue';
 import TransactionGraphicWidget from '@/components/widgets/TransactionGraphicWidget.vue';
 import AccountBalanceWidget from '@/components/widgets/AccountBalanceWidget.vue';
-import NodeRewardsClient from 'symbol-node-rewards-client';
+import NodeRewardsClient from '@/components/widgets/NodeRewardsWidget';
 import NodesMapWidget from '@/components/widgets/NodesMapWidget.vue';
 import NodeStatsWidget from '@/components/widgets/NodeStatsWidget.vue';
 
