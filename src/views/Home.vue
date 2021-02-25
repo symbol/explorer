8f59abe284db69d6023afe0789c637640839d094
Oleg Makarenko <olegomm@gmail.com>
2021-02-25 19:37:32 +0200

    fix home page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,17 +19,17 @@
 <template>
 	<b-container fluid class="px-0 py-0">
 		<b-row class="my-4 mx-0 mx-xs-0 mx-md-4 mx-lg-8">
-			<b-col xs="12" md="12" lg="9">
-				<PriceChartWidget />
+			<b-col xs="12" md="12" lg="9" style="display: flex">
+				<PriceChartWidget style="width: 100%"/>
 			</b-col>
 			<b-col xs="12" md="12" lg="3" style="display: flex">
 				<BaseInfoWidget style="width: 100%"/>
 			</b-col>
-			<b-col xs="12" md="12" lg="6">
-				<RecentBlocksWidget />
+			<b-col xs="12" md="12" lg="6" style="display: flex">
+				<RecentBlocksWidget style="width: 100%"/>
 			</b-col>
-			<b-col xs="12" md="12" lg="6">
-				<RecentTransactionsWidget />
+			<b-col xs="12" md="12" lg="6" style="display: flex">
+				<RecentTransactionsWidget style="width: 100%"/>
 			</b-col>
 		</b-row>
 	</b-container>
