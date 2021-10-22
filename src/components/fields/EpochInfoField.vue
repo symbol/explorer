e2adc00e4b770d32916c2453f8364649793ebb8a
AnthonyLaw <yc-law1015@hotmail.com>
2021-10-23 03:16:47 +0800

    feat: add epoch info component field

---
+++ src/components/fields/EpochInfoField.vue
@@ -0,0 +1,60 @@
+/*
+ *
+ * Copyright (c) 2021-present for NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License ");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+<template>
+	<div>
+		<span :class="{'colorLight': isExpired}">
+			{{ epochStatus }} : {{ epochStart }} - {{ epochEnd }}
+		</span>
+	</div>
+</template>
+<script>
+import TableView from '@/components/tables/TableView.vue';
+import Constants from '../../config/constants';
+
+export default {
+	name: 'EpochInfoField',
+	extends: TableView,
+	props: {
+		value: {
+			type: Object,
+			required: true
+		}
+	},
+	computed: {
+		epochStatus() {
+			return this.getKeyName(this.value.epochStatus.toLowerCase());
+		},
+		epochStart() {
+			return `${this.getKeyName('epoch')} ${this.value.epochStart}`;
+		},
+		epochEnd() {
+			return `${this.getKeyName('epoch')} ${this.value.epochEnd}`;
+		},
+		isExpired() {
+			return this.value.epochStatus === Constants.EpochStatus.EXPIRED;
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.colorLight {
+    opacity: 0.5;
+}
+</style>
