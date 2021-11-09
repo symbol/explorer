1c84555592c98bdd0c0d7c06885444b4fca83693
AnthonyLaw <yc-law1015@hotmail.com>
2021-11-10 01:53:22 +0800

    fix: update pagination arrow style for dark mode

--- src/components/controls/Pagination.vue
+++ src/components/controls/Pagination.vue
@@ -109,6 +109,16 @@ export default {
 .pagination-wrapper {
     float: right;
 
+	.btn-outline-info {
+		color: var(--clickable-text);
+		border-color: var(--clickable-text);
+	}
+
+	.btn-outline-info:hover {
+		color: var(--text-color);
+		background-color: var(--clickable-text);
+	}
+
     .pagination-arrow {
         justify-content: center;
         flex-direction: column;
