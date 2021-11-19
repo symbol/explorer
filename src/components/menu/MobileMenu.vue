b3263370db494db53ef5119bd5572cba3404643b
AnthonyLaw <yc-law1015@hotmail.com>
2021-11-20 03:39:34 +0800

    refactor: menu component

--- src/components/menu/MobileMenu.vue
+++ src/components/menu/MobileMenu.vue
@@ -46,16 +46,10 @@ import IconMenu from 'vue-material-design-icons/Menu.vue';
 import LanguageSelector from '@/components/controls/LanguageSelector.vue';
 import { pageMenu } from '../../config/';
 import ThemeToggle from '../ThemeToggle.vue';
-import IconStatistics from '../../styles/img/statistics.png';
-import IconMosaics from '../../styles/img/mosaic.png';
-import IconHome from '../../styles/img/home.png';
-import IconBlocks from '../../styles/img/block.png';
-import IconTransactions from '../../styles/img/transaction.png';
-import IconAccounts from '../../styles/img/account.png';
-import IconNodes from '../../styles/img/nodes.png';
-import IconNamespaces from '../../styles/img/namespace.png';
+import MenuComponent from './MenuComponent.vue';
 
 export default {
+	extends: MenuComponent,
 	components: {
 		IconMenu,
 		LanguageSelector,
@@ -73,48 +67,13 @@ export default {
 		return {
 			items: pageMenu.items,
 			showDrawer: false,
-			scrolled: true,
-			IconStatistics,
-			IconMosaics,
-			IconHome,
-			IconBlocks,
-			IconTransactions,
-			IconAccounts,
-			IconNodes,
-			IconNamespaces
+			scrolled: true
 		};
 	},
 
 	methods: {
 		toggleMenu() {
 			this.showDrawer = !this.showDrawer;
-		},
-
-		getNameByKey(e) {
-			return this.$store.getters['ui/getNameByKey'](e);
-		},
-
-		iconUrl(icon) {
-			switch (icon) {
-			case 'IconHome':
-				return this.IconHome;
-			case 'IconBlocks':
-				return this.IconBlocks;
-			case 'IconTransactions':
-				return this.IconTransactions;
-			case 'IconAccounts':
-				return this.IconAccounts;
-			case 'IconNodes':
-				return this.IconNodes;
-			case 'IconNamespaces':
-				return this.IconNamespaces;
-			case 'IconStatistics':
-				return this.IconStatistics;
-			case 'IconMosaics':
-				return this.IconMosaics;
-			default:
-				return null;
-			}
 		}
 	}
 };
--- src/components/menu/NavigationMenu.vue
+++ src/components/menu/NavigationMenu.vue
@@ -25,16 +25,11 @@
 <script>
 import { pageMenu } from '../../config/';
 import ThemeToggle from '../ThemeToggle.vue';
-import IconStatistics from '../../styles/img/statistics.png';
-import IconMosaics from '../../styles/img/mosaic.png';
-import IconHome from '../../styles/img/home.png';
-import IconBlocks from '../../styles/img/block.png';
-import IconTransactions from '../../styles/img/transaction.png';
-import IconAccounts from '../../styles/img/account.png';
-import IconNodes from '../../styles/img/nodes.png';
-import IconNamespaces from '../../styles/img/namespace.png';
+import MenuComponent from './MenuComponent.vue';
 
 export default {
+	extends: MenuComponent,
+
 	components: {
 		ThemeToggle
 	},
@@ -62,44 +57,8 @@ export default {
 		return {
 			items: pageMenu.items,
 			fixed: false,
-			scrollListener: {},
-			IconStatistics,
-			IconMosaics,
-			IconHome,
-			IconBlocks,
-			IconTransactions,
-			IconAccounts,
-			IconNodes,
-			IconNamespaces
+			scrollListener: {}
 		};
-	},
-
-	methods: {
-		getNameByKey(e) {
-			return this.$store.getters['ui/getNameByKey'](e);
-		},
-		iconUrl(icon) {
-			switch (icon) {
-			case 'IconHome':
-				return this.IconHome;
-			case 'IconBlocks':
-				return this.IconBlocks;
-			case 'IconTransactions':
-				return this.IconTransactions;
-			case 'IconAccounts':
-				return this.IconAccounts;
-			case 'IconNodes':
-				return this.IconNodes;
-			case 'IconNamespaces':
-				return this.IconNamespaces;
-			case 'IconStatistics':
-				return this.IconStatistics;
-			case 'IconMosaics':
-				return this.IconMosaics;
-			default:
-				return null;
-			}
-		}
 	}
 };
 </script>
