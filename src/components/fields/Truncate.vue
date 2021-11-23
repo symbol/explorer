d5884b1f6a985408f31221df26f23d7a19241d48
AnthonyLaw <yc-law1015@hotmail.com>
2021-11-24 06:42:21 +0800

    fix: remove truncate-mobile and add media queries

--- src/components/fields/Truncate.vue
+++ src/components/fields/Truncate.vue
@@ -3,31 +3,16 @@
 		<div class="d-none d-md-block">
 			<slot />
 		</div>
-		<div class="truncate-mobile d-block d-md-none">
+		<div class="d-block d-md-none">
 			<span><slot /></span>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
-.truncate {
-    word-break: break-all;
-}
-
-.truncate-mobile {
-    span {
-        white-space: nowrap;
-        overflow: hidden;
-        display: inline-block;
-    }
-
-    span:first-child {
-        width: 70px;
-        text-overflow: ellipsis;
-        -webkit-mask-image: linear-gradient(to left, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 1));
-        -webkit-mask-size: 100% 100%;
-        -webkit-mask-repeat: no-repeat;
-        -webkit-mask-position: left top;
+@media screen and (min-width: 764px) {
+    .truncate {
+        word-break: break-all;
     }
 }
 </style>
