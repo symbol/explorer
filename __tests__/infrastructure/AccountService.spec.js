8f30b56ed92bb48cc5548483ecbe5c128c7c0e08
Anthony Law Yong Chuan <yc-law1015@hotmail.com>
2022-02-25 02:52:13 +0800

    task: fix account service unit test

--- __tests__/infrastructure/AccountService.spec.js
+++ __tests__/infrastructure/AccountService.spec.js
@@ -1,26 +1,290 @@
-import { AccountService } from '../../src/infrastructure'
-import networkConfig from '../config/network.conf.json'
-import { constructHttp } from '../utils'
+import { AccountService, ChainService, NamespaceService, NodeService } from '../../src/infrastructure';
+import { stub } from 'sinon';
+import { NetworkType, Account, Mosaic, UInt64, MosaicId } from 'symbol-sdk';
 
-beforeAll(async() => {
-    jest.setTimeout(10000)
-    await constructHttp()
+jest.mock('../../src/infrastructure/http', () => {
+	return {
+		networkType: 152,
+		networkCurrency: {
+			divisibility: 6,
+			mosaicId: '6BED913FA20223F8',
+			namespaceId: 'E74B99BA41F4AFEE',
+			namespaceName: 'symbol.xym'
+		}
+	};
 });
 
+/**
+ * Random generate testnet account.
+ * @param {number} numberOfAccount number of account.
+ * @returns {array} Accounts
+ */
+const stubGenerateAccount = numberOfAccount => {
+	return [...Array(numberOfAccount).keys()].map(() => Account.generateNewAccount(NetworkType.TEST_NET));
+};
+
+/**
+ * Mock Account info.
+ * @param {Account} account Account.
+ * @returns {object} account info.
+ */
+const stubMockAccountInfo = account => {
+	return {
+		version: 1,
+		address: account.address.plain(),
+		addressHeight: 1,
+		publicKey: account.publicKey,
+		publicKeyHeight: 1,
+		accountType: 'Remote',
+		activityBucket: [],
+		mosaics: [],
+		importance: '0.000000 %',
+		importanceHeight: 0,
+		supplementalPublicKeys: {
+			linked: Account.generateNewAccount(NetworkType.TEST_NET).publicKey,
+			node: 'N/A',
+			vrf: 'N/A',
+			voting: [{
+				publicKey: Account.generateNewAccount(NetworkType.TEST_NET).publicKey,
+				startEpoch: 1,
+				endEpoch:200
+			},
+			{
+				publicKey: Account.generateNewAccount(NetworkType.TEST_NET).publicKey,
+				startEpoch: 201,
+				endEpoch:400
+			},
+			{
+				publicKey: Account.generateNewAccount(NetworkType.TEST_NET).publicKey,
+				startEpoch: 401,
+				endEpoch:600
+			}]
+		}
+	};
+};
+
 describe('Account Service', () => {
+	describe('getAccountInfo should', () => {
 
-    describe('getAccountInfo should', () => {
+		let getAccount = {};
+		let getChainInfo = {};
+		let getAccountsNames = [];
 
-        it('return custom account object', async () => {
-            const accountInfo = await AccountService.getAccountInfo(networkConfig.testAccount.address)
-            expect(accountInfo).not.toBeNull()
-        })
-    })
+		beforeEach(() => {
+			getAccount = stub(AccountService, 'getAccount');
+			getChainInfo = stub(ChainService, 'getChainInfo');
+			getAccountsNames = stub(NamespaceService, 'getAccountsNames');
+		});
 
-    describe('getAccountList should', () => {
-        it('return custom account list', async () => {
-            const accountList = await AccountService.getAccountList(networkConfig.pagination.pageSize, 'harvested/blocks')
-            expect(accountList).not.toBeNull()
-        })
-    })
-})
+		afterEach(() => {
+			getAccount.restore();
+			getChainInfo.restore();
+			getAccountsNames.restore();
+		});
+
+		it('return custom account object', async () => {
+			// Arrange:
+			const account = stubGenerateAccount(1)[0];
+			const mockAccountInfo = stubMockAccountInfo(account);
+
+			const mockChainInfo = {
+				latestFinalizedBlock: {
+					height: 187732,
+					hash: '6E95A894530C3E55EA8801A3715C6E247BB4B76F41721D41E21110E211E0231D',
+					finalizationPoint: 39,
+					finalizationEpoch: 300
+				}
+			};
+
+			const mockAccountAlias = [{
+				address: account.address.plain(),
+				names: [{
+					name: 'mockAddress'
+				}]
+			}];
+
+			getAccount.returns(Promise.resolve(mockAccountInfo));
+
+			getChainInfo.returns(Promise.resolve(mockChainInfo));
+
+			getAccountsNames.returns(Promise.resolve(mockAccountAlias));
+
+			// Act:
+			const accountInfo = await AccountService.getAccountInfo(account.address.plain());
+
+			// Assert:
+			expect(accountInfo.address).toEqual(account.address.plain());
+			expect(accountInfo.votingList[0].epochInfo.epochStatus).toEqual('Current');
+			expect(accountInfo.votingList[1].epochInfo.epochStatus).toEqual('Future');
+			expect(accountInfo.votingList[2].epochInfo.epochStatus).toEqual('Expired');
+			expect(accountInfo.accountAliasNames).toEqual(expect.arrayContaining(mockAccountAlias[0].names.map(names => names.name)));
+		});
+	});
+
+	describe('getAccountList should', () => {
+		const pageInfo = {
+			pageNumber: 1,
+			pageSize: 10
+		};
+		let searchAccounts = {};
+		let getStorageInfo = {};
+		let getAccountsNames = [];
+
+		beforeEach(() => {
+			searchAccounts = stub(AccountService, 'searchAccounts');
+			getAccountsNames = stub(NamespaceService, 'getAccountsNames');
+			getStorageInfo = stub(NodeService, 'getStorageInfo');
+
+			getStorageInfo.returns(Promise.resolve({
+				numAccounts: 100
+			}));
+		});
+
+		afterEach(() => {
+			searchAccounts.restore();
+			getAccountsNames.restore();
+			getStorageInfo.restore();
+		});
+
+		it('return accounts', async () => {
+			// Arrange:
+			const accounts = stubGenerateAccount(2);
+
+			const mockSearchAccounts = {
+				pageNumber: 1,
+				pageSize: 10,
+				data: accounts.map(account => {
+					return stubMockAccountInfo(account);
+				})
+			};
+
+			const mockAccountsAlias = accounts.map(account => {
+				return {
+					address: account.address.plain(),
+					names: []
+				};
+			});
+
+			searchAccounts.returns(Promise.resolve(mockSearchAccounts));
+
+			getAccountsNames.returns(Promise.resolve(mockAccountsAlias));
+
+			// Act:
+			const accountList = await AccountService.getAccountList(pageInfo, {});
+
+			// Assert:
+			expect(accountList.totalRecords).toEqual(100);
+			expect(accountList.pageNumber).toEqual(mockSearchAccounts.pageNumber);
+			expect(accountList.pageSize).toEqual(mockSearchAccounts.pageSize);
+			expect(accountList.data).toHaveLength(2);
+			accountList.data.forEach(account => {
+				expect(account).toHaveProperty('accountAliasNames');
+				expect(account).toHaveProperty('balance');
+			});
+		});
+
+		it('return accounts contain alias', async () => {
+			// Arrange:
+			const accounts = stubGenerateAccount(1);
+
+			const mockSearchAccounts = {
+				...pageInfo,
+				data: accounts.map(account => {
+					return stubMockAccountInfo(account);
+				})
+			};
+
+			const mockAccountsAlias = accounts.map((account, index) => {
+				const aliasIndex = index + 1;
+				return {
+					address: account.address.plain(),
+					names: [{
+						name: `alias_${aliasIndex}`
+					}]
+				};
+			});
+
+			searchAccounts.returns(Promise.resolve(mockSearchAccounts));
+
+			getAccountsNames.returns(Promise.resolve(mockAccountsAlias));
+
+			// Act:
+			const accountList = await AccountService.getAccountList(pageInfo, {});
+
+			// Assert:
+			accountList.data.forEach((account, index) => {
+				expect(account.accountAliasNames).toEqual([`alias_${index + 1}`]);
+			});
+		});
+
+		it('return accounts 0 balance given non network currency', async () => {
+			// Arrange:
+			const accounts = stubGenerateAccount(1);
+
+			const mockSearchAccounts = {
+				...pageInfo,
+				data: accounts.map(account => {
+					const mockAccountInfo = {
+						...stubMockAccountInfo(account),
+						mosaics: [new Mosaic(new MosaicId('19A9CFED0C0E752C'), UInt64.fromUint(1000))]
+					};
+					return mockAccountInfo;
+				})
+			};
+
+			const mockAccountsAlias = accounts.map(account => {
+				return {
+					address: account.address.plain(),
+					names: []
+				};
+			});
+
+			searchAccounts.returns(Promise.resolve(mockSearchAccounts));
+
+			getAccountsNames.returns(Promise.resolve(mockAccountsAlias));
+
+			// Act:
+			const accountList = await AccountService.getAccountList(pageInfo, {});
+
+			// Assert:
+			accountList.data.forEach(account => {
+				expect(account.balance).toEqual('0.000000');
+			});
+		});
+
+		it('return accounts balance given network currency', async () => {
+			// Arrange:
+			const accounts = stubGenerateAccount(1);
+
+			const mockSearchAccounts = {
+				...pageInfo,
+				data: accounts.map(account => {
+					const mockAccountInfo = {
+						...stubMockAccountInfo(account),
+						mosaics: [new Mosaic(new MosaicId('6BED913FA20223F8'), UInt64.fromUint(1000))]
+					};
+					return mockAccountInfo;
+				})
+			};
+
+			const mockAccountsAlias = accounts.map(account => {
+				return {
+					address: account.address.plain(),
+					names: []
+				};
+			});
+
+			searchAccounts.returns(Promise.resolve(mockSearchAccounts));
+
+			getAccountsNames.returns(Promise.resolve(mockAccountsAlias));
+
+			// Act:
+			const accountList = await AccountService.getAccountList(pageInfo, {});
+
+			// Assert:
+			accountList.data.forEach(account => {
+				expect(account.balance).toEqual('0.001000');
+			});
+		});
+	});
+});
