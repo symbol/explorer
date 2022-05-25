04ac3063df5c9875f363ff20bc3093dc8bc08556
Anthony Law <anthony@symbol.dev>
2022-05-26 02:23:24 +0800

    [explorer] feat: added account label
    * [explorer] feat: add account label in listing and detail page
    
    * [explorer] feat: add symbol accounts label list
    
    * [explorer] task: patch language
    
    * [explorer] task: rename accountsLabel to accountLabels
    
    * [explorer] task: lint fix

--- __tests__/config/jest.setup.js
+++ __tests__/config/jest.setup.js
@@ -24,6 +24,9 @@ jest.mock('../../src/infrastructure/http', () => {
 			{
 				namespaceName: 'symbol.xym'
 			}
-		]
+		],
+		accountLabels: {
+			'TBNMIAJEUCUAQYUNEUP2FPULTOQ5SJ2ZTPNWXIA': 'Mock Exchange'
+		}
 	};
 });
