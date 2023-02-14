1d2d6fef769c6f540afcc347f3856331e6514326
Anthony Law <anthony@symbol.dev>
2023-02-15 03:10:20 +0800

    [explorer] fix: search namespace without dash #1152
    * [explorer] fix: search namespace without dash
    
    * [explorer] task: refactor UI store unit test
    
    * [explorer] task: patch update on UI store

--- __tests__/store/ui.spec.js
+++ __tests__/store/ui.spec.js
@@ -1,10 +1,9 @@
-import Helper from '../../src/helper';
 import { AccountService, MosaicService, NamespaceService } from '../../src/infrastructure';
 import ui from '../../src/store/ui';
 import { stub, restore } from 'sinon';
 
 describe('store/ui', () => {
-	describe('action search should', () => {
+	describe('action search', () => {
 		let dispatch;
 		let rootGetters;
 
@@ -15,7 +14,22 @@ describe('store/ui', () => {
 
 		afterEach(restore);
 
-		it('return block page with height', async () => {
+		const assertSearchNamespace = async namespaceName => {
+			// Arrange:
+			stub(NamespaceService, 'getNamespaceInfo').returns(Promise.resolve({}));
+
+			// Act:
+			await ui.actions.search({ dispatch, rootGetters }, namespaceName);
+
+			// Assert:
+			expect(dispatch).toHaveBeenCalledTimes(1);
+			expect(dispatch).toHaveBeenNthCalledWith(1, 'openPage', {
+				pageName: 'namespace',
+				param: namespaceName
+			});
+		};
+
+		it('returns block page with height', async () => {
 			// Arrange:
 			const height = '10';
 
@@ -30,12 +44,11 @@ describe('store/ui', () => {
 			});
 		});
 
-		it('return account page with public key', async () => {
+		it('returns account page with public key', async () => {
 			// Arrange:
 			const publicKey = 'DC20B243B63246C9E75E4FB5ED236513A005454393E93C8A4CE6EDEE323C2DDB';
 
-			const getAccountInfoStub = stub(AccountService, 'getAccountInfo');
-			getAccountInfoStub.returns(Promise.resolve({}));
+			stub(AccountService, 'getAccountInfo').returns(Promise.resolve({}));
 
 			// Act:
 			await ui.actions.search({ dispatch, rootGetters }, publicKey);
@@ -48,7 +61,7 @@ describe('store/ui', () => {
 			});
 		});
 
-		it('return account page with address', async () => {
+		it('returns account page with address', async () => {
 			// Arrange:
 			const address = 'TAR5OQBKR4KSVRVZ3ZBVHNLMBZ4N4Q27WFVMJDI';
 
@@ -63,7 +76,7 @@ describe('store/ui', () => {
 			});
 		});
 
-		it('return transaction page with hash', async () => {
+		it('returns transaction page with hash', async () => {
 			// Arrange:
 			const hash = '706BBC8F95AF60B22CB38911A645D3BA24DC480FDBE18C197ACCFE0FDE0DC24D';
 
@@ -78,12 +91,11 @@ describe('store/ui', () => {
 			});
 		});
 
-		it('return mosaic page with mosaic Id', async () => {
+		it('returns mosaic page with mosaic Id', async () => {
 			// Arrange:
 			const mosaicId = '3A8416DB2D53B6C8';
 
-			const getMosaicInfoStub = stub(MosaicService, 'getMosaicInfo');
-			getMosaicInfoStub.returns(Promise.resolve({}));
+			stub(MosaicService, 'getMosaicInfo').returns(Promise.resolve({}));
 
 			// Act:
 			await ui.actions.search({ dispatch, rootGetters }, mosaicId);
@@ -96,23 +108,9 @@ describe('store/ui', () => {
 			});
 		});
 
-		it('return namespace page with namespace name', async () => {
-			// Arrange:
-			const namespaceName = 'symbol.xym';
+		it('returns namespace page with namespace name', async () => await assertSearchNamespace('symbol.xym'));
 
-			const getNamespaceInfoStub = stub(NamespaceService, 'getNamespaceInfo');
-			getNamespaceInfoStub.returns(Promise.resolve({}));
-
-			// Act:
-			await ui.actions.search({ dispatch, rootGetters }, namespaceName);
-
-			// Assert:
-			expect(dispatch).toHaveBeenCalledTimes(1);
-			expect(dispatch).toHaveBeenNthCalledWith(1, 'openPage', {
-				pageName: 'namespace',
-				param: namespaceName
-			});
-		});
+		it('returns namespace page with namespace name contain dash', async () => await assertSearchNamespace('symbol-'));
 
 		it('throw error given Nem address', async () => {
 			// Arrange:
@@ -131,6 +129,58 @@ describe('store/ui', () => {
 			return expect(ui.actions.search({ dispatch, rootGetters }, randomText))
 				.rejects.toThrow('errorNothingFound');
 		});
-	});
 
+		describe('input value contain whitespace', () => {
+			const runBasicWhitespaceTests = (pageName, inputValue, expectedResult) => {
+				it(`returns ${pageName} page`, async () => {
+					// Arrange:
+					if ('mosaic' === pageName)
+						stub(MosaicService, 'getMosaicInfo').returns(Promise.resolve({}));
+					else if('account' === pageName)
+						stub(AccountService, 'getAccountInfo').returns(Promise.resolve({}));
+					else if ('namespace' === pageName)
+						stub(NamespaceService, 'getNamespaceInfo').returns(Promise.resolve({}));
+
+					// Act:
+					await ui.actions.search({ dispatch, rootGetters }, inputValue);
+
+					// Assert:
+					expect(dispatch).toHaveBeenNthCalledWith(1, 'openPage', expectedResult);
+				});
+			};
+
+			// Arrange:
+			const inputValues = [
+				{
+					pageName: 'block',
+					input: ' 10 '
+				},
+				{
+					pageName: 'account',
+					input: ' DC20B243B63246C9E75E4FB5ED236513A005454393E93C8A4CE6EDEE323C2DDB '
+				},
+				{
+					pageName: 'account',
+					input: ' TAR5OQBKR4KSVRVZ3ZBVHNLMBZ4N4Q27WFVMJDI '
+				},
+				{
+					pageName: 'transaction',
+					input: ' 706BBC8F95AF60B22CB38911A645D3BA24DC480FDBE18C197ACCFE0FDE0DC24D'
+				},
+				{
+					pageName: 'mosaic',
+					input: '3A8416DB2D53B6C8 '
+				},
+				{
+					pageName: 'namespace',
+					input: 'symbol . xym '
+				}
+			];
+
+			inputValues.forEach(({input, pageName}) => runBasicWhitespaceTests(pageName, input, {
+				pageName,
+				param: input.replace(/\s/g, '')
+			}));
+		});
+	});
 });
